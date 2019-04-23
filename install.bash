882d0abb1561eea19f88dff529f342eb29bbab3e
nhyne <adamjohnson35@gmail.com>
2019-04-23 10:54:04 -0400

    git submodule update call

--- install.bash
+++ install.bash
@@ -1,3 +1,6 @@
+# make sure we've cloned the .tmux repo
+git submodule update
+
 # link ~/.tmux to submodlue .tmux.conf
 ln -s -f $(pwd)/.tmux/.tmux.conf ~/.tmux.conf
 ln -s -f $(pwd)/dot_files/.tmux.conf.local ~/.tmux.conf.local
